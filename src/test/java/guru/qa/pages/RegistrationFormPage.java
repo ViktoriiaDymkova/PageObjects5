6c98a4bf2efb099034d8864556eeb08cf9baa3d7
user <stel-vd@yandex.ru>
2022-04-17 14:14:54 +0300

    make with PageObjects

---
+++ src/test/java/guru/qa/pages/RegistrationFormPage.java
@@ -0,0 +1,97 @@
+package guru.qa.pages;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import com.github.javafaker.Faker;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationFormPage {
+    //locators
+
+
+    //actions
+    public RegistrationFormPage openPage(){
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        //executeJavaScript("$('footer').remote()");
+        //executeJavaScript("$('#fixe').remote()");
+        zoom(0.65);
+        return this;
+    }
+
+    public RegistrationFormPage setFirstName(String value){
+        $("#firstName").setValue(value);
+        return this;
+    }
+    public RegistrationFormPage setlastName(String value){
+        $("#lastName").setValue(value);
+        return this;
+    }
+    public RegistrationFormPage setuserEmail(String value){
+        $("#userEmail").setValue(value);
+        return this;
+    }
+    public RegistrationFormPage setGender(String value){
+        $("#genterWrapper").setValue(value);
+        return this;
+    }
+    public RegistrationFormPage setPhoneNumber(String value) {
+        $("#phone").setValue(value).click();
+        return this;
+    }
+
+    public RegistrationFormPage setDateOfBirth(String month, String year, String day){
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--0" + day).click();
+        return this;
+    }
+
+    public RegistrationFormPage setSubjects(String English) {
+        $("#subjectsInput").setValue("English").pressEnter();
+        return this;
+    }
+
+
+
+    public RegistrationFormPage getHobbies (String Music){
+    $("#hobbiesWrapper").$(byText(Music)).click();
+   return this;
+}
+    public RegistrationFormPage upLoadPicture (){
+        $("#uploadPicture").uploadFromClasspath("bruss.jpeg");
+        return this;
+    }
+    public RegistrationFormPage getCurrentAddress (String value){
+        $("#currentAddress").setValue(value);
+        return this;
+    }
+    public RegistrationFormPage setState (String value){
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(value)).click();
+        return this;
+    }
+    public RegistrationFormPage setCity (String value){
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(value)).click();
+        return this;
+    }
+    public RegistrationFormPage setsubmit() {
+        $("#submit").click();
+        return this;
+    }
+
+    public RegistrationPages checkHeader(String header, String value) {
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        return this;
+    }
+    public RegistrationFormPage checkResult(String key, String value){
+        $(".table-responsive").$(byText(key))
+            .parent().shouldHave(text(value));
+        return this;
+    }
+    }
---
+++ src/test/java/guru/qa/tests/RegistrationFormWithPageObjectsTests.java
@@ -0,0 +1,58 @@
+package guru.qa.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import guru.qa.pages.RegistrationFormPage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class RegistrationFormWithPageObjectsTests {
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    String firstName = "vika";
+    String lastName = "kom";
+    String userEmail = "vika@kom.com";
+    String genter = "Female"
+    String phone = "1234567890";
+    String subject = "English";
+    String hobbie = "Music";
+    String currentAddress = "Spb";
+    String state = "NCR";
+    String city = "Delhi";
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+    @Test
+    void fillFormTest() {
+        registrationFormPage.openPage()
+                .setFirstName(firstName)
+                .setlastName(lastName)
+                .setGender(genter)
+                .setuserEmail(userEmail)
+                .setPhoneNumber(phone)
+                .setDateOfBirth("July","1991", "011" )
+//        $(".react-datepicker__month-select").selectOption("July");
+//        $(".react-datepicker__year-select").selectOption("1991");
+//        $(".react-datepicker__day--011:not(.react-datepicker__day--outside-month)").click();
+                .setSubjects(subject)
+                .getHobbies(hobbie)
+                .upLoadPicture()
+                .getCurrentAddress(currentAddress)
+                .setState(state)
+                .setCity(city)
+                .setsubmit()
+    //       $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        registrationFormPage.checkHeader(Thanks for submitting the form)
+        registrationFormPage.checkResult("Student Name", "vika kom")
+                .checkResult("Student Email", "vika@kom.com")
+                .checkResult("Gender", "Female");
+    }
+}
